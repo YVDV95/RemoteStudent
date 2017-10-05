3f8d03fda9212d2867d41d9b37e3a1a185048997
Yorick van der Vis <yorickvandervis@ziggo.nl>
2017-10-05 13:46:33 +0200

    update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,12 +3,7 @@ class JobsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
-		if params[:category].blank?
-			@jobs = Job.all.order("created_at DESC")
-		else
-			@category_id = Category.find_by(name: params[:category]).id
-			@jobs = Job.where(category_id: @category_id).order("created_at DESC")
-		end
+		@jobs = Job.all.order("created_at DESC")
 	end
 
 	def new
